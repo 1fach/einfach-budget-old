231167324e3d831f466bd6e7d7a84ec4aca123cf
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2023-09-22 17:47:12 +0200

    ✨ init panda css

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,15 +2,16 @@ import dns from 'dns'
 
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import redwood from '@redwoodjs/vite'
 
 // See: https://vitejs.dev/config/server-options.html#server-host
 // So that Vite will load on local instead of 127.0.0.1
 dns.setDefaultResultOrder('verbatim')
 
-import redwood from '@redwoodjs/vite'
-
 const viteConfig: UserConfig = {
-  plugins: [redwood()],
+  plugins: [redwood(), tsconfigPaths()],
 }
 
 export default defineConfig(viteConfig)
