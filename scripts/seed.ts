72bd6a8e23ff8435033c41640b793fd0f0bce63e
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2023-09-26 20:23:52 +0200

    🔨 add seed scripts to populate tables

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,38 +1,223 @@
-import type { Prisma } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+import { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
+
+import { hashPassword } from '@redwoodjs/auth-dbauth-api'
+
+const deleteAllTablesData = async () => {
+  await db.transaction.deleteMany()
+  console.log('Deleted records in transaction table')
+
+  await db.account.deleteMany()
+  console.log('Deleted records in account table')
+
+  await db.payee.deleteMany()
+  console.log('Deleted records in payee table')
+
+  await db.monthlyBudgetPerCategory.deleteMany()
+  console.log('Deleted records in monthlyBudgetPerCategory table')
+
+  await db.budgetCategory.deleteMany()
+  console.log('Deleted records in budgetCategory table')
+
+  await db.budgetCategoryGroup.deleteMany()
+  console.log('Deleted records in budgetCategoryGroup table')
+
+  await db.budget.deleteMany()
+  console.log('Deleted records in budget table')
+
+  await db.user.deleteMany()
+  console.log('Deleted records in user table')
+}
+
+const populateTable = async () => {
+  console.log('========= START POPULATING TABLES =========')
+  for (let i = 0; i < 10; i++) {
+    const [hashedPassword, salt] = hashPassword(faker.internet.password())
+    const data: Prisma.UserCreateInput = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      hashedPassword,
+      salt,
+    }
+    const user = await db.user.create({ data: data })
+    console.log('Created user ' + user.id)
+
+    await createBudgetsForUser(user.id, 2)
+  }
+  console.log('========= END POPULATING TABLES =========')
+}
+
+const createBudgetsForUser = async (userId, count) => {
+  for (let i = 0; i < count; i++) {
+    const data: Prisma.BudgetUncheckedCreateInput = {
+      name: faker.lorem.words(3),
+      userId: userId,
+    }
+
+    const budget = await db.budget.create({ data: data })
+    console.log('Created budget ' + budget.id)
+
+    const accounts = await createAccountsForBudget(budget.id, 3)
+
+    const monthlyBudgets = await createBudgetCategoryGroupForBudget(
+      budget.id,
+      2
+    )
+    const flatMonthlyBudgets = monthlyBudgets.flat(4)
+
+    for (const monthlyBudget of flatMonthlyBudgets) {
+      await createTransactionFromAllAccounts(monthlyBudget.id, accounts, 2)
+    }
+  }
+}
+
+async function createTransactionFromAllAccounts(
+  monthlyBudgetId,
+  accounts,
+  count
+) {
+  console.log('== Start Create Transactions ==')
+  for (const account of accounts) {
+    const balance = 100000
+    const inflow: Prisma.TransactionUncheckedCreateInput = {
+      description: 'Inflow Balance',
+      date: faker.date.between({
+        from: '2023-09-01T00:00:00.000Z',
+        to: '2023-12-31T00:00:00.000Z',
+      }),
+      outflow: 0,
+      inflow: balance,
+      cleared: true,
+      monthlyBudgetPerCategoryId: monthlyBudgetId,
+      accountId: account.id,
+    }
+    await db.transaction.create({ data: inflow })
+    console.log('Created inflow for a account ' + account.id)
+
+    for (let i = 0; i < count; i++) {
+      const data: Prisma.TransactionUncheckedCreateInput = {
+        description: faker.finance.transactionDescription(),
+        date: faker.date.between({
+          from: '2023-09-01T00:00:00.000Z',
+          to: '2023-12-31T00:00:00.000Z',
+        }),
+        outflow: faker.finance.amount({
+          min: balance / (count + 5),
+          max: balance / count,
+        }),
+        inflow: 0,
+        cleared: faker.datatype.boolean(0.25),
+        monthlyBudgetPerCategoryId: monthlyBudgetId,
+        accountId: account.id,
+      }
+
+      const transaction = await db.transaction.create({ data: data })
+      console.log('Created transaction ' + transaction.id)
+    }
+  }
+  console.log('== End Create Transactions ==')
+}
+
+async function createBudgetCategoryGroupForBudget(budgetId, count) {
+  const created = []
+  for (let i = 0; i < count; i++) {
+    const data: Prisma.BudgetCategoryGroupUncheckedCreateInput = {
+      name: faker.commerce.department(),
+      sortOrder: faker.number.int(10000),
+      budgetId: budgetId,
+    }
+
+    const group = await db.budgetCategoryGroup.create({ data: data })
+    console.log('Created category group ' + group.id)
+
+    const monthlyBudgets = await createBudgetCategoryForGroup(group.id, 2)
+    created.push(monthlyBudgets)
+  }
+
+  return created
+}
+
+async function createBudgetCategoryForGroup(categoryGroupId, count) {
+  const created = []
+  for (let i = 0; i < count; i++) {
+    const data: Prisma.BudgetCategoryUncheckedCreateInput = {
+      name: faker.commerce.department(),
+      sortOrder: faker.number.int(10000),
+      groupId: categoryGroupId,
+    }
+
+    const category = await db.budgetCategory.create({ data: data })
+    console.log('Created category ' + category.id)
+
+    const monthlyBudgets = await createMonthlyBudgetPerCategoryForCategory(
+      category.id
+    )
+
+    created.push(monthlyBudgets)
+  }
+
+  return created
+}
+
+async function createMonthlyBudgetPerCategoryForCategory(categoryId) {
+  const created = []
+  for (let i = 9; i <= 12; i++) {
+    const data: Prisma.MonthlyBudgetPerCategoryUncheckedCreateInput = {
+      month: i,
+      year: 2023,
+      assigned: faker.finance.amount(),
+      budgetCategoryId: categoryId,
+    }
+
+    const monthlyBudget = await db.monthlyBudgetPerCategory.create({
+      data: data,
+    })
+    console.log('Created monthly budget ' + monthlyBudget.id)
+
+    created.push(monthlyBudget)
+  }
+
+  return created
+}
+
+async function createAccountsForBudget(budgetId, count) {
+  const created = []
+  for (let i = 0; i < count; i++) {
+    const accName = faker.lorem.words(3)
+
+    const payee = await db.payee.create({
+      data: {
+        name: accName,
+      },
+    })
+    console.log('Created payee ' + payee.id)
+
+    const data: Prisma.AccountUncheckedCreateInput = {
+      nickname: accName,
+      budgetId: budgetId,
+      payeeId: payee.id,
+    }
+
+    const account = await db.account.create({ data: data })
+    console.log('Created account ' + account.id)
+    created.push(account)
+  }
+
+  return created
+}
 
 export default async () => {
   try {
     //
     // Manually seed via `yarn rw prisma db seed`
     // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
-    //
-    // Update "const data = []" to match your data model and seeding needs
-    //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
-    ]
-    console.log(
-      "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
-    )
 
-    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
-    // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
-      })
-    )
+    // clear all database
+    await deleteAllTablesData()
+
+    // populate tables with relationships
+    await populateTable()
 
     // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
     // and associated `salt` to their record. Here's how to create them using
@@ -57,7 +242,6 @@ export default async () => {
     //     })
     //   }
   } catch (error) {
-    console.warn('Please define your seed data.')
     console.error(error)
   }
 }
