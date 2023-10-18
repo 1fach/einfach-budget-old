6ba6d300dc33b986d54cd7ef074c0dabf172a53c
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2023-10-18 17:56:24 +0200

    💄 add loading skeleton in sidebar

--- web/src/components/AppLayout/sidebar-section.tsx
+++ web/src/components/AppLayout/sidebar-section.tsx
@@ -2,6 +2,7 @@ import { css } from '@one-ui/styled-system/css'
 import { Folder } from 'lucide-react'
 
 import { LinkButton } from '@/ui/link-button'
+import { Skeleton } from '@/ui/skeleton'
 
 type SidebarSectionItem = {
   title: string
@@ -43,6 +44,34 @@ export const SidebarSection = ({ title, items }: SidebarSectionProps) => {
             <Folder className={css({ mr: '2', h: '4', w: '4' })} />
             {item.title}
           </LinkButton>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export const SidebarSectionSkeleton = ({ title }: { title: string }) => {
+  return (
+    <div className={css({ px: '3', py: '2' })}>
+      <h2
+        className={css({
+          mb: '2',
+          px: '4',
+          fontSize: 'lg',
+          lineHeight: 'lg',
+          fontWeight: 'semibold',
+          letterSpacing: 'tight',
+        })}
+      >
+        {title}
+      </h2>
+      <div
+        className={css({
+          my: '1',
+        })}
+      >
+        {Array.from({ length: 3 }, (_, index) => (
+          <Skeleton w="80%" h="5" my="4" mx="3" key={index} />
         ))}
       </div>
     </div>
--- web/src/components/UserBudgetsCell/UserBudgetsCell.tsx
+++ web/src/components/UserBudgetsCell/UserBudgetsCell.tsx
@@ -3,7 +3,10 @@ import type { UserBudgetsQuery } from 'types/graphql'
 import { routes } from '@redwoodjs/router'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
-import { SidebarSection } from '../AppLayout/sidebar-section'
+import {
+  SidebarSection,
+  SidebarSectionSkeleton,
+} from '../AppLayout/sidebar-section'
 
 export const QUERY = gql`
   query UserBudgetsQuery($userId: String!) {
@@ -14,7 +17,7 @@ export const QUERY = gql`
   }
 `
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = () => <SidebarSectionSkeleton title="Budgets" />
 
 export const Empty = () => <div>Empty</div>
 
