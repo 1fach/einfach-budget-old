ab597e9381ee762f8bda7d493177074e258d50cc
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-03-19 23:12:06 +0100

    ♻️ refactor user api

--- api/src/graphql/users.sdl.ts
+++ api/src/graphql/users.sdl.ts
@@ -3,39 +3,19 @@ export const schema = gql`
     id: ID!
     name: String
     email: String!
-    hashedPassword: String!
-    salt: String!
-    resetToken: String
-    resetTokenExpiresAt: DateTime
     budgets: [Budget]!
   }
 
   type Query {
-    users: [User!]! @requireAuth
-    user(id: String!): User @requireAuth
-  }
-
-  input CreateUserInput {
-    name: String
-    email: String!
-    hashedPassword: String!
-    salt: String!
-    resetToken: String
-    resetTokenExpiresAt: DateTime
+    user: User @requireAuth
   }
 
   input UpdateUserInput {
     name: String
     email: String
-    hashedPassword: String
-    salt: String
-    resetToken: String
-    resetTokenExpiresAt: DateTime
   }
 
   type Mutation {
-    createUser(input: CreateUserInput!): User! @requireAuth
-    updateUser(id: String!, input: UpdateUserInput!): User! @requireAuth
-    deleteUser(id: String!): User! @requireAuth
+    updateUser(input: UpdateUserInput!): User! @requireAuth
   }
 `
--- api/src/services/users/users.ts
+++ api/src/services/users/users.ts
@@ -6,32 +6,16 @@ import type {
 
 import { db } from 'src/lib/db'
 
-export const users: QueryResolvers['users'] = () => {
-  return db.user.findMany()
-}
-
-export const user: QueryResolvers['user'] = ({ id }) => {
+export const user: QueryResolvers['user'] = () => {
   return db.user.findUnique({
-    where: { id },
+    where: { id: context.currentUser?.id },
   })
 }
 
-export const createUser: MutationResolvers['createUser'] = ({ input }) => {
-  return db.user.create({
-    data: input,
-  })
-}
-
-export const updateUser: MutationResolvers['updateUser'] = ({ id, input }) => {
+export const updateUser: MutationResolvers['updateUser'] = ({ input }) => {
   return db.user.update({
     data: input,
-    where: { id },
-  })
-}
-
-export const deleteUser: MutationResolvers['deleteUser'] = ({ id }) => {
-  return db.user.delete({
-    where: { id },
+    where: { id: context.currentUser?.id },
   })
 }
 
