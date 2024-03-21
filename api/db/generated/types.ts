9dd6f17bf849b0001501c77e54d89a5f722b5e04
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-03-21 03:23:56 +0100

    🧑‍💻 add kysely to be used as query builder

---
+++ api/db/generated/types.ts
@@ -0,0 +1,72 @@
+import type { ColumnType } from 'kysely'
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>
+export type Timestamp = ColumnType<Date, Date | string, Date | string>
+
+export type Account = {
+  id: string
+  nickname: string
+  budget_id: string
+  payee_id: string
+}
+export type Budget = {
+  id: string
+  name: string
+  user_id: string
+}
+export type BudgetCategory = {
+  id: string
+  name: string
+  sort_order: number
+  budget_category_group_id: string
+}
+export type BudgetCategoryGroup = {
+  id: string
+  name: string
+  sort_order: number
+  budget_id: string
+}
+export type MonthlyBudgetPerCategory = {
+  id: string
+  month: number
+  year: number
+  assigned: Generated<string>
+  budget_category_id: string
+}
+export type Payee = {
+  id: string
+  name: string
+  budget_id: string
+}
+export type Transaction = {
+  id: string
+  description: string
+  date: Generated<Timestamp>
+  outflow: Generated<string>
+  inflow: Generated<string>
+  cleared: Generated<boolean>
+  updated_at: Timestamp
+  account_id: string
+  payee_id: string | null
+  monthly_budget_per_category_id: string
+}
+export type User = {
+  id: string
+  name: string | null
+  email: string
+  hashed_password: string
+  salt: string
+  reset_token: string | null
+  reset_token_expires_at: Timestamp | null
+}
+export type DB = {
+  account: Account
+  budget: Budget
+  budget_category: BudgetCategory
+  budget_category_group: BudgetCategoryGroup
+  monthly_budget_per_category: MonthlyBudgetPerCategory
+  payee: Payee
+  transaction: Transaction
+  user: User
+}
--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -2,8 +2,17 @@
 // for options.
 
 import { PrismaClient } from '@prisma/client'
+import {
+  Kysely,
+  PostgresAdapter,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+} from 'kysely'
+import kyselyExtension from 'prisma-extension-kysely'
 
 import { emitLogLevels, handlePrismaLogging } from '@redwoodjs/api/logger'
+
+import type { DB } from '../../db/generated/types'
 
 import { logger } from './logger'
 
@@ -12,7 +21,19 @@ import { logger } from './logger'
  */
 export const db = new PrismaClient({
   log: emitLogLevels(['info', 'warn', 'error']),
-})
+}).$extends(
+  kyselyExtension({
+    kysely: (driver) =>
+      new Kysely<DB>({
+        dialect: {
+          createDriver: () => driver,
+          createAdapter: () => new PostgresAdapter(),
+          createIntrospector: (db) => new PostgresIntrospector(db),
+          createQueryCompiler: () => new PostgresQueryCompiler(),
+        },
+      }),
+  })
+)
 
 handlePrismaLogging({
   db,
