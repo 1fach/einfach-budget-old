24f2abc8e1babd9b7648eba2ec62ea88d34da32a
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-03-24 14:32:11 +0100

    ✨ add progress bar between page transition

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -1,5 +1,10 @@
+import { useEffect, useState } from 'react'
+
+import { NProgress } from '@einfach-ui/react'
 import { css } from '@einfach-ui/styled/css'
 import { Grid, GridItem } from '@einfach-ui/styled/jsx'
+
+import { useLocation } from '@redwoodjs/router'
 
 import { NavigationMenu, Sidebar } from 'src/components/AppLayout'
 
@@ -8,22 +13,39 @@ type AppLayoutProps = {
 }
 
 const AppLayout = ({ children }: AppLayoutProps) => {
+  const [progress, setProgress] = useState(false)
+  const [prevLoc, setPrevLoc] = useState({})
+  const location = useLocation()
+
+  useEffect(() => {
+    setPrevLoc(location)
+    setProgress(true)
+    window.scrollTo(0, 0) //Go to top on every page load
+  }, [location])
+
+  useEffect(() => {
+    setProgress(false)
+  }, [prevLoc])
+
   return (
-    <div className={css({ display: 'none', md: { display: 'block' } })}>
-      <NavigationMenu />
-      <div>
-        <div className={css({ bg: 'background' })}>
-          <Grid gridTemplateColumns={{ lg: '6' }}>
-            <Sidebar
-              className={css({ display: 'none', lg: { display: 'block' } })}
-            />
-            <GridItem colSpan={5} borderLeftWidth={{ lg: '1px' }}>
-              {children}
-            </GridItem>
-          </Grid>
+    <>
+      <NProgress isAnimating={progress} />
+      <div className={css({ display: 'none', md: { display: 'block' } })}>
+        <NavigationMenu />
+        <div>
+          <div className={css({ bg: 'background' })}>
+            <Grid gridTemplateColumns={{ lg: '6' }}>
+              <Sidebar
+                className={css({ display: 'none', lg: { display: 'block' } })}
+              />
+              <GridItem colSpan={5} borderLeftWidth={{ lg: '1px' }}>
+                {children}
+              </GridItem>
+            </Grid>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
