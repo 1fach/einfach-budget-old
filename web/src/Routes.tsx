509a90752c1b12ab682e59f2f0f6765cc926df67
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-03-24 14:53:36 +0100

    🎨 remove unused set

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route, PrivateSet } from '@redwoodjs/router'
+import { Router, Route, PrivateSet } from '@redwoodjs/router'
 
 import AppLayout from 'src/layouts/AppLayout/AppLayout'
 
@@ -21,14 +21,11 @@ const Routes = () => {
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Route path="/oops" page={NotFoundPage} name="oops" />
-
-      <PrivateSet unauthenticated="login">
-        <Set wrap={AppLayout}>
-          <Route path="/" page={HomePage} name="home" />
-          <Route path="/budget/{budget}" page={RedirectBudgetPage} name="budget" />
-          <Route path="/budget/{budget}/{slug}" page={BudgetPage} name="budgetSlug" />
-          <Route path="/account/{account}" page={AccountPage} name="account" />
-        </Set>
+      <PrivateSet unauthenticated="login" wrap={AppLayout}>
+        <Route path="/" page={HomePage} name="home" />
+        <Route path="/budget/{budget}" page={RedirectBudgetPage} name="budget" />
+        <Route path="/budget/{budget}/{slug}" page={BudgetPage} name="budgetSlug" />
+        <Route path="/account/{account}" page={AccountPage} name="account" />
       </PrivateSet>
       <Route notfound page={NotFoundPage} />
     </Router>
